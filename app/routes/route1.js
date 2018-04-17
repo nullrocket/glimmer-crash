a46b62916fa5eae879827e650690c5cb1721708d
Doug <douglas.b.hill@gmail.com>
2018-04-17 00:38:33 -0700

    Initial

---
+++ app/routes/route1.js
@@ -0,0 +1,15 @@
+import Route from '@ember/routing/route';
+import {later} from '@ember/runloop';
+export default Route.extend({
+  count:0,
+  actions:{
+    didTransition(){
+      later(this,()=>{
+        this.incrementProperty('count');
+        if(this.get('count') < 100) {
+          this.transitionTo('route2');
+        }
+    },50);
+    }
+  }
+});
